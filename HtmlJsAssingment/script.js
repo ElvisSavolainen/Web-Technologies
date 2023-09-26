7d18cc4597163bf2f2dac571d7aa27da96a80ee8
Elvis Savolainen <dead.savo@gmail.com>
2023-09-26 17:14:56 +0300

    Todo List now works
    - You can add elements
    - You can delete elements
    - You can checkout elements
    - You can not add blank elements (this is limited so that no one can spam)

--- HtmlJsAssingment/script.js
+++ HtmlJsAssingment/script.js
@@ -1,32 +1,54 @@
-// Add new todo task to todoList
-function addTodo() {
-    // TODO: Get the value from todoInput element
-    var todoInput = document.getElementsByTagName("todoInput");
-for (var i = 0; i < inputs.length; i++) {
-  alert(todoInput[i].id);
+var button = document.querySelector("#button");
+var todoList = document.querySelector("#todoList");
+var input = document.querySelector("#todoInput");
+
+button.addEventListener("click", function(e) {
+  e.preventDefault();
+  
+  if (input.value === '') {
+    alert("You must write something!");
+  } else {
+      var li = document.createElement("LI"); // create an li
+      li.textContent = input.value; // set its text content to input's value
+      todoList.appendChild(li); // add it to the ul
+  }
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";}}
+});
+
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
 }
-    // TODO: Check that input value was provided
 
-    // TODO: Get todoList element from html and assign it to variable
-    var list = document.getElementById('todoList');
-    // TODO: Create new li element and assign it to variable
-
-    // TODO: Assing the todoInput value to the textContent
-
-    // TODO: Create completed button to set the task to done, createElement
-
-    // TODO: Add text to the completed button using textContent
-
-    // TODO: Add onclick method to the completed button, that changes the background color of the todo item
-    // Below is one way to change the background color using ternary logic
-    // todoListItem.style.backgroundColor = todoListItem.style.backgroundColor == "green" ? "" : "green";
-
-    // TODO: Add created done button to new todo list item
-
-    // TODO: Add new todo list item to todoList
-
-    // TODO: Set todoInput value to empty string
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
 }
 
-// Extra assingment -> try to find a way to remove all completed todo items
-// hint -> you might need to use additional list variable
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
